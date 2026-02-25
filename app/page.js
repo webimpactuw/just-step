3340075fcb8fec29d9235ddb538681870cce17db
Jason Nguyen <138557364+jason17nguyen@users.noreply.github.com>
2026-02-24 16:35:40 -0800

    Implement navbar and hero per high-fidelity

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <Footer />
-    </div>
+    <main>
+      <Hero />
+    </main>
   );
 }
